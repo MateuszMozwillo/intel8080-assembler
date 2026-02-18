012c3fa27df20bd97e1ec22e3909816905a8da05
mateusz <mateusz.mozwillo@gmail.com>
2026-02-18 15:49:45 +0100

    added functionality to specify input/output filename from cli

--- src/main.c
+++ src/main.c
@@ -1,5 +1,7 @@
 #include "assembler.h"
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 int save_bytecode(const char *filename, ByteCode *bc) {
     FILE *f = fopen(filename, "wb");
@@ -15,12 +17,28 @@ int save_bytecode(const char *filename, ByteCode *bc) {
     return 1;
 }
 
-int main() {
+#define MAX_FILENAME_LEN 20
+int main(int argc, char *argv[]) {
 
-    const char *filename = "main.asm";
+    if (argc != 3) {
+        printf("invalid argument count specify input and output file");
+        exit(EXIT_FAILURE);
+    }
+
+    char input_filename[MAX_FILENAME_LEN];
+    char output_filename[MAX_FILENAME_LEN];
+
+    if (strlen(argv[1]) > MAX_FILENAME_LEN || strlen(argv[2]) > MAX_FILENAME_LEN) {
+        printf("filename too long");
+        exit(EXIT_FAILURE);
+    }
+
+    strcpy(input_filename, argv[1]);
+    strcpy(output_filename, argv[2]);
+
     char *bfr = 0;
     size_t file_length;
-    FILE *f = fopen(filename, "rb");
+    FILE *f = fopen(input_filename, "rb");
 
     if (f) {
         fseek(f, 0, SEEK_END);
@@ -42,7 +60,7 @@ int main() {
         printf("0x%x\n", assembled.bytes[i]);
     }
 
-    save_bytecode("main.bin", &assembled);
+    save_bytecode(output_filename, &assembled);
 
     free(assembled.bytes);
     free(bfr);
