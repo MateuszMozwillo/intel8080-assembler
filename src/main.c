0a8d5bedae51115606e527d1130dd1e64222c754
mateusz <mateusz.mozwillo@gmail.com>
2026-03-08 14:09:02 +0100

    changes

--- src/main.c
+++ src/main.c
@@ -25,42 +25,38 @@ int main(int argc, char *argv[]) {
         exit(EXIT_FAILURE);
     }
 
-    char input_filename[MAX_FILENAME_LEN];
-    char output_filename[MAX_FILENAME_LEN];
-
-    if (strlen(argv[1]) > MAX_FILENAME_LEN || strlen(argv[2]) > MAX_FILENAME_LEN) {
-        printf("filename too long");
-        exit(EXIT_FAILURE);
-    }
-
-    strcpy(input_filename, argv[1]);
-    strcpy(output_filename, argv[2]);
-
     char *bfr = 0;
     size_t file_length;
-    FILE *f = fopen(input_filename, "rb");
+    FILE *f = fopen(argv[1], "rb");
 
-    if (f) {
-        fseek(f, 0, SEEK_END);
-        file_length = ftell(f);
-        fseek(f, 0, SEEK_SET);
-
-        bfr = malloc(file_length + 1);
-        if (bfr) {
-            fread(bfr, 1, file_length, f);
-            bfr[file_length] = '\0';
-        }
-
-        fclose(f);
+    if (!f) {
+        printf("ERROR: cant open file\n");
+        return 1;
     }
+
+    fseek(f, 0, SEEK_END);
+    file_length = ftell(f);
+    fseek(f, 0, SEEK_SET);
+
+    bfr = malloc(file_length + 1);
+    if (bfr) {
+        fread(bfr, 1, file_length, f);
+        bfr[file_length] = '\0';
+    }
+
+    fclose(f);
 
     ByteCode assembled = generate_byte_code(bfr);
 
-    for (size_t i = 0; i < assembled.len; i++) {
-        printf("0x%x\n", assembled.bytes[i]);
-    }
+    // for (size_t i = 0; i < assembled.len; i++) {
+    //     printf("0x%x\n", assembled.bytes[i]);
+    // }
 
-    save_bytecode(output_filename, &assembled);
+    if (!save_bytecode(argv[2], &assembled)) {
+        printf("ERROR: cant save byte code\n");
+    } else {
+        printf("byte code generated\n");
+    }
 
     free(assembled.bytes);
     free(bfr);
